9470d7dfcf81f5713075eac7f3855c724c1baa74
Anuska <ank.sampedro@gmail.com>
2019-07-09 12:18:58 +0200

    overview

--- index.js
+++ index.js
@@ -2,26 +2,57 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');const dataObj = JSON.parse(data);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRITION%}/g, product.nutrients);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    if (!product.organic) {
+        output = output.replace(/{%NOTORGANIC%}/g, 'not-organic');
+    }
+    return output;
+}
 
 const server = http.createServer((req, res) => {
     const pathName = req.url;
     //console.log(req.url);
     //res.end('Hello from the server');
     if (pathName === '/' || pathName === '/overview') {
-        res.end('This is the overview')
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+
+        const cardsHTML = dataObj.map(el => replaceTemplate(tempCard, el));
+        const output = tempOverview.replace('{%CARDS%}', cardsHTML);
+        res.end(output);
+
     } else if (pathName === '/product') {
-        res.end('These are the products')
+        res.writeHead(200, {
+            'Content-type': 'text/html'});
+        res.end(tempProduct)
+
     } else if (pathName === '/api') {
         // fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => { 
         //     const productData = JSON.parse(data);
-            res.writeHead(200, {'Content-type': 'application/json'});
-            //console.log(productData);
-            res.end(data);
+        res.writeHead(200, {
+            'Content-type': 'application/json'});
+        //console.log(productData);
+        res.end(data);
+
     } else {
         res.writeHead(404, {
-            'Content-type': 'text/html',
-        });
+            'Content-type': 'text/html',});
         res.end('<h1>Page not found</h1>')
     }
 });
@@ -30,5 +61,3 @@ const server = http.createServer((req, res) => {
 server.listen(8000, '127.0.0.1', () => {
     console.log('The server has been started')
 })
-
-
